440acba11481719e725e2bb84e1f7e4420ecbfd5
unknown <mido.elnaggar@gmail.com>
2019-08-24 15:00:01 +0200

    Updates

---
+++ precache-manifest.e466dc9922ecce2a296904add014b4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b769a3366315f9fca2ad6429a6eeb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d4714985f997d484f93",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "95a0bc359c37275d0841",
+    "url": "./static/js/2.cb9efb0e.chunk.js"
+  },
+  {
+    "revision": "8d4714985f997d484f93",
+    "url": "./static/js/main.8dca4b44.chunk.js"
+  },
+  {
+    "revision": "fb15d146fe71f20bb318",
+    "url": "./static/js/runtime~main.348ff770.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
